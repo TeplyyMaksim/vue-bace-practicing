bf9ba98d65d932e2e679e6c1f6c90393485f450a
Maksim Teplyy <maksim.teplyy@teamvoy.com>
2017-05-18 19:34:07 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <input type="text" @input="changeTitle">
+    </div>
+    <div v-once>
+      Initial title: {{title}}
+    </div>
+    <div>
+      Single title: {{title}}
+    </div>
+    <div>
+      Square title: {{generator()}}
+    </div>
+    <hr>
+    <div>
+      I did it megasite using <a :href="link" target="_blank" @click="resetTitle">this</a> site
+    </div>
+    <div>
+      It is <span v-html="htmledLink"></span>, if you do not got a Joke
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      title: 'Rewrite me using right input',
+      link: 'http://google.com',
+      htmledLink: '<a href="http://google.com" target="_blank">Google</a>',
+    };
+  },
+  methods: {
+    changeTitle(event) {
+      this.title = event.target.value;
+    },
+    generator() {
+      return `${this.title} ${this.title}`;
+    },
+    resetTitle() {
+      this.title = 'Reseted';
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
